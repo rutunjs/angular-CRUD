a7d5394c721a285a5fbc78c9b066f6ea15431ea0
unknown <rutunjs@gmail.com>
2018-07-19 00:58:56 +0530

    animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {FormsModule} from '@angular/forms';
+// import { BrowserAnimationModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
