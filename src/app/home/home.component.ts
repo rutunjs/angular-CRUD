385e47dad48b01f9c17b24f0f429e0bbc5f1e7e5
unknown <rutunjs@gmail.com>
2018-07-19 00:46:58 +0530

    array demo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount : number =4 ;
+  btnText : string = "Add item values";
+  goalText : string = "";
+  goals = [];
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    console.table;
+    this.goals.push(this.goalText);
+    this.goalText = "";
+    this.itemCount = this.goals.length;
+    
   }
 
 }
